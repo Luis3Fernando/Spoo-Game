ddf1582d00f0fc7f713da8c893da6d67c118099b
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2023-06-03 22:51:21 -0500

    feat: solucionando interaccion del boton

--- spoo_world/world/inicio.gd
+++ spoo_world/world/inicio.gd
@@ -10,3 +10,8 @@ func _on_TouchScreenButton_pressed():
 	var new_scene_path = "res://spoo_world/world/Admin/Login.tscn"  # Ruta a la nueva escena
 	get_tree().change_scene(new_scene_path)
 	
+
+
+func _on_Button_pressed():
+	var new_scene_path = "res://spoo_world/world/levels/level_main.tscn"  # Ruta a la nueva escena
+	get_tree().change_scene(new_scene_path)
