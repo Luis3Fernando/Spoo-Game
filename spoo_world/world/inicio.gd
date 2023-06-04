8adf48390c3133c2d0b0a5a9de37f3314472a1bb
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2023-06-03 23:45:55 -0500

    refactor: comentando lineas

--- spoo_world/world/inicio.gd
+++ spoo_world/world/inicio.gd
@@ -1,10 +1,28 @@
 extends Node2D
 
 var count=0
-var items = ["Elemento 1", "Elemento 2", "Elemento 3"] 
-func _ready():
+var students = [
+	"Luis Flores Torres",
+	"Ana González Vega",
+	"Diego Rodríguez Paredes",
+	"Valentina Pérez Ramos",
+	"Carlos Torres Silva",
+	"Isabella Vargas López",
+	"Sebastián Sánchez Huamaní",
+	"Camila Díaz Castro",
+	"Javier Silva Mendoza",
+	"Lucía Chávez Quispe"
+] 
+func _ready():	
+	#for i in range(len(students)):
+	#	var object_student = Button.new()
+	#	#add_child(object_student)
+	#	object_student.text = "\t\t" + str(i) + "\t\t\t" + students[i]
+	#	object_student.icon = load("res://spoo_world/assets/icon/arrow_50.png")
+	#	object_student.align = Button.ALIGN_LEFT
+	#	object_student.icon_align = Button.ALIGN_RIGHT
+	#	$VBoxContainer.add_child(object_student)
 	pass
-
 
 func _on_TouchScreenButton_pressed():
 	var new_scene_path = "res://spoo_world/world/Admin/Login.tscn"  # Ruta a la nueva escena
