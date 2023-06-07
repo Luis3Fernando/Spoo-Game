858cf072785005ae1e4bf351c35a0b06eddbb6cc
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2023-06-07 00:44:28 -0500

    fix: arreglando escena principal de usuario

--- spoo_world/world/inicio.gd
+++ spoo_world/world/inicio.gd
@@ -20,8 +20,6 @@ var colorHover = Color(0.4, 0.7, 1.0)  # Ejemplo: Azul claro más brillante
 var colorPressed = Color(0.1, 0.3, 0.6)  # Ejemplo: Azul oscuro
  
 func _ready():	
-<<<<<<< HEAD
-	pass
 	#for i in range(len(students)):
 	#	var object_student = Button.new()
 	#	#add_child(object_student)
@@ -30,7 +28,6 @@ func _ready():
 	#	object_student.align = Button.ALIGN_LEFT
 	#	object_student.icon_align = Button.ALIGN_RIGHT
 	#	$VBoxContainer.add_child(object_student)
-=======
 	for i in range(len(students)):
 		var object_student = Button.new()
 		#add_child(object_student)
@@ -43,7 +40,6 @@ func _ready():
 		object_student.add_color_override("pressed_color", colorPressed)
 		get_tree().get_root().get_node("inicio/ScrollContainer/VBoxContainer").add_child(object_student)
 		#$VBoxContainer.add_child(object_student)
->>>>>>> 6687275696c59f513434f784d997f2157055d736
 	
 	numHijos = get_tree().get_root().get_node("inicio/ScrollContainer/VBoxContainer").get_child_count()
 	asignar_eventos()
