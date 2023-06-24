f09078a44f4638306acd62224a6c18faf464f807
elizabeth-0303 <elizabethmaquera03@gmail.com>
2023-06-24 09:56:07 -0500

    feat: Agregando la interfaz del administrador

--- spoo_world/world/levels/level_main.gd
+++ spoo_world/world/levels/level_main.gd
@@ -15,26 +15,21 @@ func _ready():
 #func _process(delta):
 #	pass
 
-
 func _on_Button_pressed():
 	var new_scene_path = "res://spoo_world/world/levels/level1.tscn"  # Ruta a la nueva escena
 	get_tree().change_scene(new_scene_path)
-
 
 func _on_Button2_pressed():
 	var new_scene_path = "res://spoo_world/world/levels/level2.tscn"  # Ruta a la nueva escena
 	get_tree().change_scene(new_scene_path)
 
-
 func _on_Button3_pressed():
 	var new_scene_path = "res://spoo_world/world/levels/level3.tscn"  # Ruta a la nueva escena
 	get_tree().change_scene(new_scene_path)
 
-
 func _on_Button4_pressed():
 	var new_scene_path = "res://spoo_world/world/levels/level4.tscn"  # Ruta a la nueva escena
 	get_tree().change_scene(new_scene_path)
-
 
 func _on_Button5_pressed():
 	var new_scene_path = "res://spoo_world/world/levels/level5.tscn"  # Ruta a la nueva escena
