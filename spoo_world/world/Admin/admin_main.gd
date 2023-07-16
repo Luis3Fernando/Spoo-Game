c9807f4226fd1a09a77f0ffd97cd846b7203f226
elizabeth-0303 <elizabethmaquera03@gmail.com>
2023-07-16 10:43:16 -0500

    refactor: Volviendo a conectar los paneles del administrador

--- spoo_world/world/Admin/admin_main.gd
+++ spoo_world/world/Admin/admin_main.gd
@@ -6,3 +6,19 @@ func _ready():
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://spoo_world/world/Admin/Login.tscn")
+
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://spoo_world/world/Admin/progress.tscn")
+
+
+func _on_ButtonEdit_pressed():
+	get_tree().change_scene("res://spoo_world/world/Admin/edit.tscn")
+
+func _on_ButtonDelete_pressed():
+	get_tree().change_scene("res://spoo_world/world/Admin/delete.tscn")
+
+
+func _on_ButtonAdd_pressed():
+	get_tree().change_scene("res://spoo_world/world/Admin/add.tscn")
