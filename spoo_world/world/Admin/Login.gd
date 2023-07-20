2c250b0c0a4d753425badc1cd83259d675ffc133
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2023-07-20 15:38:26 -0500

    mmmmm...

--- spoo_world/world/Admin/Login.gd
+++ spoo_world/world/Admin/Login.gd
@@ -6,7 +6,8 @@ onready var usuario_input = $Panel/Usuario2/Usuario
 onready var password_input = $Panel/Password/Password2
 
 func _ready():
-	password_input.secret = true
+	#password_input.secret = true
+	pass
 
 func _on_JUGAR_pressed():	
 	if usuario_input.text == usuario and password_input.text == password:
